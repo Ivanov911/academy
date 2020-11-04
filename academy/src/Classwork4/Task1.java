76ed3983198c43e2e0d867422c8df9621734d51b
GREY <GREY>
2020-11-04 21:12:21 +0300

    cl

---
+++ academy/src/Classwork4/Task1.java
@@ -0,0 +1,17 @@
+package Classwork4;
+
+
+
+public class Task1 {
+
+	public static void main(String[] args) {
+					
+		for (int a = 10; a < 20; a++) {
+		
+		System.out.println (a*a);
+		
+
+	}
+
+}
+	}
---
+++ academy/src/Classwork4/Task2.java
@@ -0,0 +1,46 @@
+package Classwork4;
+
+import java.util.Scanner;
+
+public class Task2 {
+	
+	
+	
+	static int calculateFactorial(int n){
+		int result = 1;
+		for (int i = 1; i <=n; i ++){
+			result = result*i;
+		}
+		return result;
+	}	
+ 
+	public static void main(String[] args){
+		System.out.println(calculateFactorial(6));
+	
+	
+	
+
+//	public static void main(String[] args) {
+	//	Scanner sc = new Scanner(System.in);
+	//	System.out.println("¬ведите число n: ");
+	//	int result = sc.nextInt();
+		//int result = 1;
+	//	for (int i = 1; i <=n; i ++){
+//			result = result*i;
+	//	}
+//		return result;
+		
+//		System.out.println (result);
+		
+		
+		
+	//	System.out.println("¬ведите число n: ");
+		//int n = sc.nextInt();
+	//	sc.close();
+	//	for (int a = 1; a == n ; a++) {
+			
+	//				System.out.println (n!);
+//	}
+
+}
+}
