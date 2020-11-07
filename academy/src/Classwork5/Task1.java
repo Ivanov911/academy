879dad5966f732d69dad3ebb003189e44d19ff99
GREY <GREY>
2020-11-07 15:23:46 +0300

    Class

---
+++ academy/src/Classwork5/Task1.java
@@ -0,0 +1,14 @@
+package Classwork5;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		String s1 = "I like Java!!!";
+	//	char s1 = String.charAt(1);
+		//System.out.println(System.out.println("charAt(1));
+				s1 = s1.replace('a', 'o');
+						System.out.println(s1);
+		s1 = s1.contains(“Java”);
+	}
+
+}
