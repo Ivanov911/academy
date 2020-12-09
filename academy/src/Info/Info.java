02a882255073ce995d22581cb8712c8bca2dcfaf
Asus <Asus@LAPTOP-78EUHOP1>
2020-12-09 09:04:10 +0300

    HM4

--- academy/src/Info/Info.java
+++ academy/src/Info/Info.java
@@ -3,7 +3,9 @@ package Info;
 public class Info {
 
 	public static void main(String[] args) {
-		// github.com/dmitry-academy/academy_5220/blob/master/academy5220/resources/tasks/9.Интерфейсы. ENUM.docx
+		// github.com/dmitry-academy/academy_5220/blob/master/academy5220/resources/tasks/9.пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ. ENUM.docx
 		// https://docs.google.com/spreadsheets/d/1eh8zSEkvAsJya-KDYLUHtNPr6x3utpyVwn9JOT_pnPQ/edit#gid=0
+		
+		//jk
 	}
 }
